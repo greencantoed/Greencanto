90da856604cc38149bdab9af60f7dd12bd4f9475
Walras <el.cumitini@proton.me>
2024-09-11 18:04:39 +0200

    fixed landing private hopefully

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['placeholder.com'], // Add any domains you'll be loading images from
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    },
+    i18n: {
+      locales: ['it'], // Add any additional locales you plan to support
+      defaultLocale: 'it',
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
