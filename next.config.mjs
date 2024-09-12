b66033d47d999aab52065e9070a4a10b2c50e4b6
Walras <el.cumitini@proton.me>
2024-09-12 19:11:21 +0200

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' https://js.stripe.com https://api.mapbox.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*.mapbox.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' https://js.stripe.com https://api.mapbox.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*.mapbox.com; connect-src 'self' https://api.mapbox.com",
           },
           {
             key: 'X-Frame-Options',
